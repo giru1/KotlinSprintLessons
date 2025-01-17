52839db5291ac64301381ca6f8f19898b667df43
giru <yagofarov.vadim@mail.ru>
2025-01-17 13:57:20 +0500

    KS-1-1 task lesson 1-1 variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_1
 
 fun main() {
-    println("fghfgh gdfgdfgd")
+
+    val numberOfOrders = 5
+
+    val thankYouMessage = "Спасибо за вашу покупку! Мы ценим ваш выбор и надеемся на дальнейшее сотрудничество."
+    println(numberOfOrders)
+    println(thankYouMessage)
 }
