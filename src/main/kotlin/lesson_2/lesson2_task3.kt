750a4e39459939b02f85bf83b83d7997a17719c7
giru <yagofarov.vadim@mail.ru>
2025-01-19 23:04:16 +0500

    KS-2-3-variables fix constants

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_2
 
+const val MINUTE_IN_HOUR: Int = 60
+const val HOURS_IN_DAY: Int = 24
+
 fun main() {
     // Время выезда (часы и минуты)
     val departureHour: Int = 9
@@ -8,21 +11,18 @@ fun main() {
     // Время в пути в минутах
     val travelTimeMinutes: Int = 457
 
-    val minuteInHour: Int = 60
-    val hoursInDay: Int = 24
-
     // Общие минуты выезда
-    val totalDepartureMinutes: Int = departureHour * minuteInHour + departureMinute
+    val totalDepartureMinutes: Int = departureHour * MINUTE_IN_HOUR + departureMinute
 
     // Общие минуты прибытия
     val totalArrivalMinutes: Int = totalDepartureMinutes + travelTimeMinutes
 
     // Вычисляем часы и минуты прибытия
-    val arrivalHour: Int = totalArrivalMinutes / minuteInHour
-    val arrivalMinute: Int = totalArrivalMinutes % minuteInHour
+    val arrivalHour: Int = totalArrivalMinutes / MINUTE_IN_HOUR
+    val arrivalMinute: Int = totalArrivalMinutes % MINUTE_IN_HOUR
 
     // Приводим часы к 24-часовому формату
-    val formattedArrivalHour: Int = arrivalHour % hoursInDay
+    val formattedArrivalHour: Int = arrivalHour % HOURS_IN_DAY
 
     // Вывод результата
     println("Время прибытия поезда: $formattedArrivalHour:${String.format("%02d", arrivalMinute)}")
