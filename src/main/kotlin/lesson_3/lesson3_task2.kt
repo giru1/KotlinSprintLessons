61a183821e5789d6427f9ce8824facce57c72c32
giru <yagofarov.vadim@mail.ru>
2025-01-20 13:26:28 +0500

    KS-3-2-string task-2

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,7 +1,6 @@
 package org.example.lesson_3
 
 fun main() {
-    // Исходные данные на момент 20 лет
     val firstName = "Татьяна"
     val middleName = "Сергеевна"
     var lastName = "Андреева"
@@ -10,7 +9,6 @@ fun main() {
     // Вывод данных в 20 лет
     println("[$lastName $firstName $middleName, $age]")
 
-    // Обновление данных после замужества
     lastName = "Сидорова"
     age = 22
 
