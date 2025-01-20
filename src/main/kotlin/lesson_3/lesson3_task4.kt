3cc27a6327ea1ffb4f2e948ae38387b02466d589
giru <yagofarov.vadim@mail.ru>
2025-01-20 22:36:52 +0500

    KS-3-4-string task-4

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,21 +1,18 @@
 package org.example.lesson_3
 
 fun main() {
-    // Инициализация переменных для первого хода
-    val from = "E2"          // Откуда
-    val to = "E4"            // Куда
-    val moveNumber = 1       // Номер хода
 
-    // Формирование строки для отправки  фигуры
+    val from = "E2"
+    val to = "E4"
+    val moveNumber = 1
+
     val moveString = "[$from-$to;$moveNumber]"
-    println(moveString)      // Вывод первого хода
+    println(moveString)
 
-    // Присвоение новых данных для следующего хода
-    val newFrom = "D2"       // Новая позиция откуда
-    val newTo = "D3"         // Куда (фигура на D2 продвигается на одну клетку вперед)
-    val newMoveNumber = 2    // Номер следующего хода
+    val newFrom = "D2"
+    val newTo = "D3"
+    val newMoveNumber = 2
 
-    // Формирование строки для следующего хода
     val newMoveString = "[$newFrom-$newTo;$newMoveNumber]"
-    println(newMoveString)    // Вывод следующего хода
+    println(newMoveString)
 }
