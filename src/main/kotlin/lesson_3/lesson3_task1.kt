1cabde031beb9772a63d23060ccb730b6783b49b
giru <yagofarov.vadim@mail.ru>
2025-01-20 13:20:23 +0500

    KS-3-1-string task-1

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Вадим"
+    val greeting = "Добрый"
+
+    // Дневное приветствие
+    println("$greeting день, $userName!")
+
+    // Вечернее приветствие
+    println("$greeting вечер, $userName!")
+}
+
